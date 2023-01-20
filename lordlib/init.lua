2ea6680c1765834af56bf341672cf90b729d3467
Alexander Chibrikin <alek13.me@gmail.com>
2023-01-20 16:45:21 +0300

    move `lordlib` into `Core` folder

---
+++ lordlib/init.lua
@@ -0,0 +1,48 @@
+lord = {}
+
+function lord.require_intllib()
+	if minetest.global_exists("intllib") then
+		return intllib.make_gettext_pair()
+	else
+		return function(q) return q end
+	end
+end
+
+-- Вспомогательная функция give_or_drop
+-- Даёт предмет данному игроку или кидает на землю рядом при недостатке места в инвентаре.
+-- Принимает:
+-- - player - объект игрока;
+-- - stack - объект ItemStack (не itemstring!).
+-- Возвращает
+-- true, если предмет положен в инвентарь, и
+-- false, если предмет выброшен.
+function lord.give_or_drop(player, stack)
+	local inv = player:get_inventory()
+	if inv:room_for_item("main", stack) then
+		inv:add_item("main", stack)
+		return true
+	else
+		minetest.item_drop(stack, player, player:get_pos())
+		return false
+	end
+end
+
+-- Вспомогательная функция each_value_equals
+-- Циклично сравнивает значение каждого элемента таблицы table с value (по умолчанию true). Если какое-то из значений
+-- таблицы не равно value — функция завершается, возвращая false. В ином случае, успешно дойдя до конца таблицы,
+-- возвращает true.
+function lord.each_value_equals(table, value)
+	if not value then
+		value = true
+	end
+	for _, v in pairs(table) do
+		if v ~= value then
+			return false
+		end
+	end
+	return true
+end
+
+
+dofile(minetest.get_modpath("lordlib").."/ru_lower_upper.lua") -- string.upper and string.lower russian symbols support
+dofile(minetest.get_modpath(minetest.get_current_modname()) .. "/core_functions.lua")
